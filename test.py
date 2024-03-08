527619f3649d52dcc537c4e102ed4b85761c458c
HmJobIs <pipeline1process@naver.com>
2024-03-08 10:48:06 +0900

    find, + all을 이용한 html 요소 검색

--- test.py
+++ test.py
@@ -12,20 +12,37 @@ html = '''
 <nav class="menu-box-1" id= "menu-box">
   <ul>
     <li>
-      <a href="https://www.naver.com">네이버로 이동</a>
+      <a class="menu-item-text" href="https://www.naver.com">네이버로 이동</a>
     </li>
     <li>
-      <a href="https://www.naver.com">다음으로 이동</a>
+      <a class="menu-item-text" href="https://www.naver.com">다음으로 이동</a>
     </li>
   </ul>
 </nav> 
 '''
-# BeautifulSoup을 사용하여 HTML을 파싱합니다.
-soup = BeautifulSoup(html, 'html.parser')
-#select() 다가져오는거 selext(one)은 검색된 하나를 가져오는것
-a_tags = soup.select('a')
-a_tag = soup.select_one('a')
-print(a_tag)
 
+#select() 다가져오는거 selext(one)은 검색된 하나를 가져오는것
+
+# 모든 일치하는 요소로 접근할 때 사용, 리스트 반환
+#a_tags = soup.select('a')
+
+# 첫 번째로 일치하는 단일 요소의 접근
+#a_tag = soup.select_one('a')
+#print(a_tag)
+
+#해당 엘리먼트가 품고 있는 텍스트 추출
 #for tags in a_tags:
 #  print(tags.get_text())
+  
+#해당 엘리먼트가 가지고 있는 href 속성값을 출력
+#for tags in a_tags:
+#  print(tags.get('href'))
+
+# BeautifulSoup을 사용하여 HTML을 파싱합니다.
+soup = BeautifulSoup(html, 'html.parser')
+
+# find_all
+menu_item_text = soup.find_all('a', class_='menu-item-text')
+
+for el in menu_item_text:
+  print(el.get('text'))
